38246d29bee84f522cfbf06b04f3274a5aa30e20
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-11-23 15:08:39 +0100

    finish exercise

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+var url = "https://mocki.io/v1/765b2daf-41d5-4e94-a0d5-abd918f57b8f";
+var Articolo = /** @class */ (function () {
+    function Articolo(_id, _codprod, _collezione, _capo, _modello, _quantita, _colore, _prezzoivaesclusa, _prezzoivainclusa, _disponibile, _saldo) {
+        this.id = _id;
+        this.codprod = _codprod;
+        this.collezione = _collezione;
+        this.capo = _capo;
+        this.modello = _modello;
+        this.quantita = _quantita;
+        this.colore = _colore;
+        this.prezzoivaesclusa = _prezzoivaesclusa;
+        this.prezzoivainclusa = _prezzoivainclusa;
+        this.disponibile = _disponibile;
+        this.saldo = _saldo;
+    }
+    Articolo.prototype.getSaldoCapo = function () {
+        return (this.prezzoivainclusa * this.saldo) / 100;
+    };
+    Articolo.prototype.getAcquistoCapo = function () {
+        console.log("sconto" + this.getSaldoCapo());
+        return parseInt((this.prezzoivainclusa - this.getSaldoCapo()).toFixed(2));
+    };
+    return Articolo;
+}());
+fetch(url)
+    .then(function (response) {
+    if (!response.ok) {
+        throw new Error("Errore HTTP! Status: ".concat(response.status));
+    }
+    return response.json();
+})
+    .then(function (data) {
+    console.log(data);
+    var array = data;
+    array.forEach(function (element) {
+        var id = element.id, codprod = element.codprod, collezione = element.collezione, capo = element.capo, modello = element.modello, quantita = element.quantita, colore = element.colore, prezzoivaesclusa = element.prezzoivaesclusa, prezzoivainclusa = element.prezzoivainclusa, disponibile = element.disponibile, saldo = element.saldo;
+        var articolo = new Articolo(id, codprod, collezione, capo, modello, quantita, colore, prezzoivaesclusa, prezzoivainclusa, disponibile, saldo);
+        console.log(articolo);
+        console.log(articolo.getAcquistoCapo());
+    });
+})
+    .catch(function (error) {
+    console.error("Si è verificato un errore durante la richiesta:", error);
+});
---
+++ assets/script.ts
@@ -0,0 +1,96 @@
+let url: string = "https://mocki.io/v1/765b2daf-41d5-4e94-a0d5-abd918f57b8f";
+
+class Articolo {
+  id: number;
+  codprod: number;
+  collezione: string;
+  capo: string;
+  modello: number;
+  quantita: number;
+  colore: string;
+  prezzoivaesclusa: number;
+  prezzoivainclusa: number;
+  disponibile: string;
+  saldo: number;
+
+  constructor(
+    _id: number,
+    _codprod: number,
+    _collezione: string,
+    _capo: string,
+    _modello: number,
+    _quantita: number,
+    _colore: string,
+    _prezzoivaesclusa: number,
+    _prezzoivainclusa: number,
+    _disponibile: string,
+    _saldo: number
+  ) {
+    this.id = _id;
+    this.codprod = _codprod;
+    this.collezione = _collezione;
+    this.capo = _capo;
+    this.modello = _modello;
+    this.quantita = _quantita;
+    this.colore = _colore;
+    this.prezzoivaesclusa = _prezzoivaesclusa;
+    this.prezzoivainclusa = _prezzoivainclusa;
+    this.disponibile = _disponibile;
+    this.saldo = _saldo;
+  }
+
+  getSaldoCapo(): number {
+    return (this.prezzoivainclusa * this.saldo) / 100;
+  }
+
+  getAcquistoCapo(): number {
+    console.log("sconto" + this.getSaldoCapo());
+    return parseInt((this.prezzoivainclusa - this.getSaldoCapo()).toFixed(2));
+  }
+}
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Errore HTTP! Status: ${response.status}`);
+    }
+
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    let array = data;
+    array.forEach(element => {
+      const {
+        id,
+        codprod,
+        collezione,
+        capo,
+        modello,
+        quantita,
+        colore,
+        prezzoivaesclusa,
+        prezzoivainclusa,
+        disponibile,
+        saldo
+      } = element;
+      let articolo = new Articolo(
+        id,
+        codprod,
+        collezione,
+        capo,
+        modello,
+        quantita,
+        colore,
+        prezzoivaesclusa,
+        prezzoivainclusa,
+        disponibile,
+        saldo
+      );
+      console.log(articolo);
+      console.log(articolo.getAcquistoCapo());
+    });
+  })
+  .catch(error => {
+    console.error("Si è verificato un errore durante la richiesta:", error);
+  });
